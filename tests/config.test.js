f5bdabd96f4df42f4006161d8f3b09e455809e6c
Nikita Gusarov <nickgusarov@gmail.com>
2018-03-08 17:35:14 +0300

    Implement getWatchList method

---
+++ tests/config.test.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const Config = require('../lib/config');
+const { promisify } = require('../lib/utils');
+
+const originalConfigPath = './tests/mocks/config.json';
+const duplicateConfigPath = './tests/mocks/config-2.json';
+const createReadStream = fs.createReadStream;
+const createWriteStream = fs.createWriteStream;
+const removeFile = promisify(fs.unlink);
+
+beforeEach(() => {
+  return new Promise((resolve, reject) => {
+    const stream = createReadStream(originalConfigPath).pipe(createWriteStream(duplicateConfigPath));
+
+    stream.on('close', resolve);
+    stream.on('error', reject);
+  });
+});
+
+afterEach(() => {
+  return removeFile(duplicateConfigPath);
+});
+
+describe('#getWatchList', () => {
+  test('resolves in watch list', () => {
+    expect.assertions(1);
+
+    const config = new Config(duplicateConfigPath);
+
+    return expect(config.getWatchList()).resolves.toEqual([
+      "A",
+      "B",
+      "C",
+      "D",
+      "E"
+    ]);
+  });
+});
