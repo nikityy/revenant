b7a48c704c9e97b305b17660f7437febaec75fa2
Nikita Gusarov <nickgusarov@gmail.com>
2020-02-02 08:25:34 +0300

    Add list command tests

---
+++ tests/revenant.test.js
@@ -0,0 +1,75 @@
+const axios = require("axios");
+const MockAdapter = require("axios-mock-adapter");
+const fs = require("fs");
+const path = require("path");
+const RutrackerApi = require("rutracker-api");
+
+const KinopoiskWatchlist = require("../lib/kinopoisk-watchlist");
+const { runRevenant } = require("../lib/revenant");
+
+const htmlPath = path.join(__dirname, "kinopoisk-movies-list.html");
+const moviesListHtml = fs.readFileSync(htmlPath, { encoding: "utf8" });
+
+const mock = new MockAdapter(axios);
+
+mock
+  .onGet("https://www.kinopoisk.ru/user/789114/movies/list/type/3575/#list")
+  .reply(200, moviesListHtml);
+
+class InMemoryConfig {
+  constructor(config) {
+    this.config = config;
+  }
+
+  async readConfig() {
+    return this.config;
+  }
+
+  async saveConfig(config) {
+    this.config = config;
+  }
+}
+
+class InMemoryLogger {
+  constructor() {
+    this.lines = [];
+  }
+
+  log(...args) {
+    this.lines.push(...args);
+  }
+}
+
+describe("list", () => {
+  test("should print all watchlist entries", async () => {
+    const config = {
+      downloadPath: "~/Downloads/",
+      rutracker: {
+        cookie: "bb-cookie:123"
+      },
+      kinopoisk: {
+        watchUrl:
+          "https://www.kinopoisk.ru/user/789114/movies/list/type/3575/#list"
+      },
+      version: "1",
+      torrents: {
+        a: ["1", "2", "3"],
+        b: ["4", "5"],
+        c: ["6", "7", "1"]
+      },
+      watchlist: ["revenant", "who's afraid of virginia woolf"]
+    };
+
+    const dependencies = {
+      configAdapter: new InMemoryConfig(config),
+      logger: new InMemoryLogger(),
+      rutracker: new RutrackerApi(),
+      watchlistClient: KinopoiskWatchlist
+    };
+
+    await runRevenant(["node", "revenant", "list"], dependencies);
+
+    expect(dependencies.logger.lines).toMatchSnapshot();
+    expect(dependencies.configAdapter.config).toMatchSnapshot();
+  });
+});
