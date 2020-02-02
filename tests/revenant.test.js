860f026f31966605dfdf6a29469723f3f68dd8e6
Nikita Gusarov <nickgusarov@gmail.com>
2020-02-02 09:09:38 +0300

    Add check command test

--- tests/revenant.test.js
+++ tests/revenant.test.js
@@ -3,12 +3,23 @@ const MockAdapter = require("axios-mock-adapter");
 const fs = require("fs");
 const path = require("path");
 const RutrackerApi = require("rutracker-api");
+const { URL } = require("url");
 
 const KinopoiskWatchlist = require("../lib/kinopoisk-watchlist");
 const { runRevenant } = require("../lib/revenant");
 
-const htmlPath = path.join(__dirname, "kinopoisk-movies-list.html");
-const moviesListHtml = fs.readFileSync(htmlPath, { encoding: "utf8" });
+const moviesListHtml = fs.readFileSync(
+  path.join(__dirname, "kinopoisk-movies-list.html"),
+  { encoding: "utf8" }
+);
+const searchResultsHtml = fs.readFileSync(
+  path.join(__dirname, "search-results-page.html"),
+  { encoding: "utf8" }
+);
+const noResultsHtml = fs.readFileSync(
+  path.join(__dirname, "no-results-page.html"),
+  { encoding: "utf8" }
+);
 
 const mock = new MockAdapter(axios);
 
@@ -19,6 +30,19 @@ mock
 mock
   .onPost("http://rutracker.org/forum/login.php")
   .reply(() => [302, null, { "set-cookie": ["bb-token=xxx"] }]);
+
+mock
+  .onPost(new RegExp("http://rutracker.org/forum/tracker.php"))
+  .reply(config => {
+    const url = new URL(config.url);
+    const query = url.searchParams.get("nm");
+
+    if (query === "Офицер и шпион Роман Полански") {
+      return [200, searchResultsHtml];
+    }
+
+    return [200, noResultsHtml];
+  });
 
 class InMemoryConfig {
   constructor(config) {
@@ -43,6 +67,42 @@ class InMemoryLogger {
     this.lines.push(...args);
   }
 }
+
+describe("check", () => {
+  test("should print new torrents", async () => {
+    const config = {
+      downloadPath: "~/Downloads/",
+      rutracker: {
+        cookie: "bb-cookie:123"
+      },
+      kinopoisk: {
+        watchUrl:
+          "https://www.kinopoisk.ru/user/789114/movies/list/type/3575/#list"
+      },
+      version: "1",
+      torrents: {
+        a: ["1", "2", "3"],
+        b: ["4", "5"],
+        c: ["6", "7", "1"]
+      },
+      watchlist: ["revenant", "who's afraid of virginia woolf"]
+    };
+
+    const dependencies = {
+      configAdapter: new InMemoryConfig(config),
+      logger: new InMemoryLogger(),
+      rutracker: new RutrackerApi(),
+      watchlistClient: KinopoiskWatchlist
+    };
+
+    dependencies.rutracker.pageProvider.request = axios;
+
+    await runRevenant(["node", "revenant", "check"], dependencies);
+
+    expect(dependencies.logger.lines).toMatchSnapshot();
+    expect(dependencies.configAdapter.config).toMatchSnapshot();
+  });
+});
 
 describe("list", () => {
   test("should print all watchlist entries", async () => {
@@ -71,6 +131,8 @@ describe("list", () => {
       watchlistClient: KinopoiskWatchlist
     };
 
+    dependencies.rutracker.pageProvider.request = axios;
+
     await runRevenant(["node", "revenant", "list"], dependencies);
 
     expect(dependencies.logger.lines).toMatchSnapshot();
@@ -78,7 +140,7 @@ describe("list", () => {
   });
 });
 
-describe.skip("login", () => {
+describe("login", () => {
   test("should update cookie in config", async () => {
     const config = {
       downloadPath: "~/Downloads/",
@@ -106,7 +168,6 @@ describe.skip("login", () => {
     };
 
     dependencies.rutracker.pageProvider.request = axios;
-    axios.defaults.adapter = mock;
 
     await runRevenant(
       ["node", "revenant", "login", "-u", "username", "-p", "password"],
